d18bb1677f79c2be3e52d806591a27e77a4d8f78
ishaaq <a.ishaaq@icloud.com>
2026-02-21 15:35:48 +0000

    fsdf

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 18:21:00 by isahmed           #+#    #+#             */
-/*   Updated: 2026/02/21 15:29:31 by isahmed          ###   ########.fr       */
+/*   Updated: 2026/02/21 15:34:24 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name")
 {
-	// this->name_ = name;
+	this->name_ = name;
 	this->hit_ = 100;
 	this->energy_ = 50;
 	this->damage_ = 30;
 	std::cout << "DiamondTrap was constructed" << std::endl;
 }
 
-DiamondTrap::DiamondTrap(void) : ClapTrap("_clap_name")
+DiamondTrap::DiamondTrap(void) : ClapTrap("no_name_clap_name")
 {
+	this->name_ = "no_name";
 	this->hit_ = 100;
 	this->energy_ = 50;
 	this->damage_ = 30;
@@ -56,6 +57,7 @@ void	DiamondTrap::attack(const std::string& target)
 
 DiamondTrap	&DiamondTrap::operator=(const DiamondTrap &rhs)
 {
+	this->name_ = rhs.name_;
 	this->hit_ = rhs.hit_;
 	this->energy_ = rhs.energy_;
 	this->damage_ = rhs.damage_;
