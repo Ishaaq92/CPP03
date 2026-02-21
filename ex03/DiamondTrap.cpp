f689cd9347a9f5d3e28d25e141e9cb72b305f18d
ishaaq <a.ishaaq@icloud.com>
2026-02-21 15:41:18 +0000

    fsdfs

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 18:21:00 by isahmed           #+#    #+#             */
-/*   Updated: 2026/02/21 15:34:24 by isahmed          ###   ########.fr       */
+/*   Updated: 2026/02/21 15:40:56 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,12 +55,15 @@ void	DiamondTrap::attack(const std::string& target)
 	ScavTrap::attack(target);
 }
 
-DiamondTrap	&DiamondTrap::operator=(const DiamondTrap &rhs)
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &rhs)
 {
-	this->name_ = rhs.name_;
-	this->hit_ = rhs.hit_;
-	this->energy_ = rhs.energy_;
-	this->damage_ = rhs.damage_;
-
-	return (*this);
+    if (this != &rhs)
+    {
+        ClapTrap::operator=(rhs);
+        this->name_ = rhs.name_;
+		this->hit_ = rhs.hit_;
+		this->energy_ = rhs.energy_;
+		this->damage_ = rhs.damage_;
+    }
+    return (*this);
 }
